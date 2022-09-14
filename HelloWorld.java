1f402f5697d56a42296cf8f0f4f1b546847c3f80
Shubham01123 <kaushik.shubh95@gmail.com>
2022-09-14 10:09:59 +0000

    First java programe

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,16 @@
-// the hello world program
-console.log('Hello World');
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Before the script...</p>
+
+  <script>
+    alert( 'Hello, world!' );
+  </script>
+
+  <p>...After the script.</p>
+
+</body>
+
+</html>
